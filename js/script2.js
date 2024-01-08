5e70e782d36b4c7ccd1891bc2de8ae61642612e8
WeslleyRennan <99375478+WeslleyRennan@users.noreply.github.com>
2024-01-08 12:42:54 -0300

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,55 @@
+const menu = document.querySelector('header')
+
+function activeScroll() {
+    menu.classList.toggle('ativo', scrollY > 0) 
+}
+
+window.addEventListener('scroll', activeScroll)
+
+let hamburger = document.querySelector('.hamburger')
+let boxnav = document.querySelector('.box-nav')
+
+hamburger.addEventListener('click', function() {
+
+    if(boxnav.style.transform === 'translate(5px, -60px)') {
+        boxnav.style.transform = 'translate(-252px, -60px)'
+
+
+        linha1.style.transform = 'rotate(0deg) translate(.0rem, .0rem)'
+        linha1.style.background = 'white'
+        linha2.style.opacity = '1'
+        linha3.style.transform = 'rotate(-0deg) translate(.0rem, -.0rem)'
+        linha3.style.background = 'white'
+
+    }else{
+        boxnav.style.transform = 'translate(5px, -60px)'
+
+
+        linha1.style.transform = 'rotate(45deg) translate(.10rem, .8rem)'
+        linha1.style.background = 'red'
+        linha2.style.opacity = '0'
+        linha3.style.transform = 'rotate(-45deg) translate(.10rem, -.8rem)'
+        linha3.style.background = 'red'
+    }
+
+})
+
+let lupa = document.querySelector('.lupa')
+let boxpesquisa = document.querySelector('.box-pesquisa')
+
+lupa.addEventListener('click', function() {
+
+    if(boxpesquisa.style.transform === 'translate(-60px, 13px)') {
+        boxpesquisa.style.transform = 'translate(-60px, -140px)'
+
+        lupa.style.width ='28px'
+        lupa.style.height ='28px'
+
+    }else{
+        boxpesquisa.style.transform = 'translate(-60px, 13px)'
+
+        lupa.style.width ='30px'
+        lupa.style.height ='30px'
+    }
+
+})
